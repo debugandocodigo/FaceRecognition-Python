b2a105d6ce1106d215e5f5ec80137d1a638f040b
Debugando Código <debugandocodigo@gmail.com>
2023-11-25 20:12:36 -0300

    Feat: Initial Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# FaceRecognition - Python
+
+## Descrição
+Este projeto tem como objetivo realizar o reconhecimento facial em tempo real utilizando Python.
+
+## Imagens de Treinamento
+As imagens utilizadas para o treinamento do modelo estão na pasta `pessoas`.
+
+## Requirements
+- Python 3.6+
+- OpenCV 4.5.1
+- Face Recognition 1.3.0
+
+## Criar Virtual Environment
+```bash
+python -m venv venv
+```
+
+## Ativar Virtual Environment
+```bash
+source venv/bin/activate
+```
+
+## Instalação das Dependências
+```bash
+pip install -r requirements.txt
+```
+
+## Uso
+```bash
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+import cv2
+import face_recognition
+
+
+known_face_encodings = []  # Lista com os encodings dos rostos conhecidos
+known_face_names = []  # Lista com os nomes dos rostos conhecidos
+
+# Função para carregar as imagens de pessoas conhecidas
+def load_images():
+    for filename in os.listdir('pessoas'):
+        # Carregar a imagem
+        image = face_recognition.load_image_file('pessoas/' + filename)
+        # Extrair o encoding do rosto
+        face_encoding = face_recognition.face_encodings(image)[0]
+        # Adicionar o encoding e o nome da pessoa
+        known_face_encodings.append(face_encoding)
+        known_face_names.append(filename.split('.')[0])
+
+
+# Função principal
+def init():
+    load_images()  # Carregar as imagens de pessoas conhecidas
+
+    video_capture = cv2.VideoCapture(2) # Inicializar a câmera
+
+    while True:
+        ret, frame = video_capture.read() # Capturar cada frame da câmera
+
+        frame = cv2.flip(frame, 1)  # Espelhar o frame horizontalmente
+
+        # Encontrar rostos no frame
+        face_locations = face_recognition.face_locations(frame)
+        face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+        # Loop sobre cada rosto encontrado no frame
+        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+
+            # Verificar se o rosto é um rosto conhecido
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+            name = "Desconhecido"  # Se não for um rosto conhecido, então o nome será "Desconhecido"
+
+            # Se um rosto conhecido for encontrado, use o primeiro
+            # (Pode ajustar isso para mostrar mais informações se houver mais de um rosto conhecido)
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = known_face_names[first_match_index]
+
+            # Desenhar um retângulo em torno do rosto e exibir o nome
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+
+        # Exibir o resultado
+        cv2.imshow('Video', frame)
+
+        # Se pressionar 'q', sair do loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+# Iniciar o programa
+init()
